61d13433ac8e3c49a3cc2a4b17c7628da3a8c590
dkac14 <150403101+dkac14@users.noreply.github.com>
2025-08-21 18:11:06 -0500

    Proyecto Base con todas las clases integradas

--- PetDayCare/src/main/java/com/mycompany/petdaycare/ChainOfResponsability/ManejadorIncidenteBase.java
+++ PetDayCare/src/main/java/com/mycompany/petdaycare/ChainOfResponsability/ManejadorIncidenteBase.java
@@ -15,9 +15,5 @@ public abstract class ManejadorIncidenteBase implements ManejadorIncidente {
         }
     }
 
-    /*El sistema de ges,ón de reparaciones “ReparaciónApp” es una aplicación basada en consola que
-organiza el flujo de reparaciones de celulares desde que los disposi,vos son recibidos por el
-técnico principal hasta su asignación a los técnicos especializados y la posterior entrega a los
-clientes. Además, el técnico principal se encarga de ges,onar cualquier incidente relacionado con
-las reparaciones; en caso de no poder resolverlos, los escala al gerente del servicio técnico. */
+
 }
