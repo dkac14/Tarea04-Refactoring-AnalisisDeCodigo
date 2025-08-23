db2ef985e9b4c03e484b8e9e97d60d586c8e0e4d
Joshua Mero (Lau) <ariegavi@espol.edu.ec>
2025-08-23 12:54:01 -0500

    Re adaptación de FabricaEntrenamientoTest post refactoring´s

--- PetDayCare/src/test/java/com/mycompany/petdaycare/Servicio/FabricaServicioEntrenamientoTest.java
+++ PetDayCare/src/test/java/com/mycompany/petdaycare/Servicio/FabricaServicioEntrenamientoTest.java
@@ -1,7 +1,10 @@
 package com.mycompany.petdaycare.Servicio;
 
+import com.mycompany.petdaycare.CategoriaCuidado.CategoriaCuidado;
 import com.mycompany.petdaycare.Composite.Servicio;
-import com.mycompany.petdaycare.Composite.ServicioEntrenamiento;
+import com.mycompany.petdaycare.Composite.ServicioBase;
+import com.mycompany.petdaycare.Composite.TipoServicio;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,7 +19,12 @@ class FabricaServicioEntrenamientoTest {
         Servicio s = fabrica.createServicio();
 
         assertNotNull(s, "El servicio no debe ser null");
-        assertTrue(s instanceof ServicioEntrenamiento, "Debe ser instancia de ServicioEntrenamiento");
+        assertTrue(s instanceof ServicioBase, "Debe ser instancia de ServicioBase");
+        assertEquals(TipoServicio.ENTRENAMIENTO.getNombre(), s.getNombre());
+        assertEquals(TipoServicio.ENTRENAMIENTO.getDescripcion(), s.getDescripcion());
+        assertEquals(TipoServicio.ENTRENAMIENTO.getPrecio(), s.getPrecio());
+        assertEquals(TipoServicio.ENTRENAMIENTO.getMoneda(), s.getCurrency());
+        assertEquals(CategoriaCuidado.BASICA, s.getCategoria());
     }
 
     
@@ -30,8 +38,13 @@ class FabricaServicioEntrenamientoTest {
         assertNotNull(s1);
         assertNotNull(s2);
         assertNotSame(s1, s2, "Cada invocación debe devolver una instancia diferente");
-        assertTrue(s1 instanceof ServicioEntrenamiento);
-        assertTrue(s2 instanceof ServicioEntrenamiento);
+        assertTrue(s1 instanceof ServicioBase);
+        assertTrue(s2 instanceof ServicioBase);
+
+        assertEquals(s1.getNombre(), s2.getNombre());
+        assertEquals(s1.getDescripcion(), s2.getDescripcion());
+        assertEquals(s1.getPrecio(), s2.getPrecio());
+        assertEquals(s1.getCurrency(), s2.getCurrency());
     }
 
     @Test
